daa8f49a5ae773fe42cb7366dfd91e21fd13e23f
Daniel Spencer <dspencer@assessor.lacounty.gov>
2019-01-09 13:06:04 -0800

    init commit

---
+++ testApplication2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace testApplication2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
